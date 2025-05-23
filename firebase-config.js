57842259275d655874c1613dcc40f09c7b2c11ad
Jin199x <148673623+Jin199x@users.noreply.github.com>
2025-05-24 02:55:07 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,16 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBXRwHdHZDwqq2eb2cLFXR1arrsO6bXRDA",
   authDomain: "feelspace-7.firebaseapp.com",
   projectId: "feelspace-7",
-  storageBucket: "feelspace-7.firebasestorage.app",
   messagingSenderId: "3685822280",
-  appId: "1:3685822280:web:a1e99ec4d597cf248b4d7d"
+  appId: "1:3685822280:web:a1e99ec4d597cf248b4d7d",
   measurementId: "G-GGSWFV3GKM"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
